b71db9602844887d35182e0e903a7108df05dff8
Mukul <mukulsharma262002@gmail.com>
2025-02-21 23:49:38 +0530

    Fixed linting

--- models/User.js
+++ models/User.js
@@ -4,6 +4,6 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
     password: String
-  });
+});
 
 module.exports = mongoose.model('User', userSchema);
