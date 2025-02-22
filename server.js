c7b8d6bd054aba08fbfa68964b8fea5b66d9f649
Mukul <mukulsharma262002@gmail.com>
2025-02-22 21:13:55 +0530

    Modify server export for Vercel serverless deployment

--- server.js
+++ server.js
@@ -20,5 +20,4 @@ app.get("/", (req, res) => {
 });
 app.use("/api", authRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
