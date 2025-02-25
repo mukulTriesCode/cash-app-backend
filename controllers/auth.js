33fcea346bce93cb1c8d58f6344145c90b28b926
Mukul <mukulsharma262002@gmail.com>
2025-02-25 22:23:17 +0530

    Created controller for register

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+//Register Controller
+const registerController = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error registering user" });
+  }
+};
+
+module.exports = { registerController };
