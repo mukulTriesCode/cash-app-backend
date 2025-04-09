4ff2a15ab26cb58f118d5360ec9d0e30640f2029
Mukul <mukulsharma262002@gmail.com>
2025-04-09 23:13:43 +0530

    Updated auth middleware to retrieve full user data upon token verification, improving error handling for missing and invalid tokens.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,26 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: "Access denied" });
+const authMiddleware = async (req, res, next) => {
+  const token = req.header("Authorization");
 
-    try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
-        next();
-    } catch (error) {
-        res.status(400).json({ error: "Invalid token" });
+  if (!token) {
+    return res.status(401).json({ error: "Access denied, No token provided" });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(verified.id).select("-password"); // remove password
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
+
+    req.user = user; // attaching full user data to req.user
+    next();
+  } catch (error) {
+    res.status(400).json({ error: "Invalid token" });
+  }
 };
 
 module.exports = authMiddleware;
