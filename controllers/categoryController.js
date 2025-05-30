bac4a5c66c0787d6289121a8a7d3d3d4a53576f0
Mukul <mukulsharma262002@gmail.com>
2025-05-30 18:08:04 +0530

    Standardize response format for getCategories endpoint

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,7 +4,10 @@ const getCategories = async (req, res) => {
   try {
     const userId = req.user.id;
     const categories = await Category.find({ userId }).select("_id name");
-    res.status(200).json(categories);
+    res.status(200).json({
+      success: true,
+      data: categories,
+    });
   } catch (err) {
     console.error("Get Categories Error:", err);
     res.status(500).json({ error: "Something went wrong" });
